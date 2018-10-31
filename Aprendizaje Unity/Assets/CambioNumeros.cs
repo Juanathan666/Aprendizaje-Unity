d8a55a17006142f5ed19511293bec884cb892c25
Unknown <delaverga69@hotmail.com>
2018-11-01 00:37:16 +0100

    Arreglo Codigo

--- Aprendizaje Unity/Assets/CambioNumeros.cs
+++ Aprendizaje Unity/Assets/CambioNumeros.cs
@@ -26,15 +26,28 @@ public class CambioNumeros : MonoBehaviour {
         if (Num > -1 && Num < 100)
         {
             Num = Num + 2;
+
+            if (Num > 100)
+            {
+                Num = 100;
+            }
+
             Texto.text = Num + "";
         }
+
     }
 
     public void Restar ()
     {
         if (Num > 0 && Num < 101)
         {
-            Num= Num -2;
+            Num = Num - 2;
+
+            if (Num == -1)
+            {
+                Num = 0;
+            }
+
             Texto.text = "" + Num;
         }
     }
