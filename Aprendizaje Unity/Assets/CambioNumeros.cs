7bb521909ea84ab7f3b7197db4f420632351cc94
Unknown <delaverga69@hotmail.com>
2018-11-01 00:02:00 +0100

    Primera prueba de Unity

---
+++ Aprendizaje Unity/Assets/CambioNumeros.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambioNumeros : MonoBehaviour {
+
+    public GameObject Numero;
+
+    Text Texto;
+
+    int Num;
+
+    void Start ()
+    {
+        Num = 1;
+
+        Texto = Numero.GetComponent<Text>();
+    }
+
+    public void Sumar ()
+    {
+        if (Num > -1 && Num < 100)
+        {
+            Num ++;
+            Texto.text = Num + "";
+        }
+    }
+
+    public void Restar ()
+    {
+        if (Num > 0 && Num < 101)
+        {
+            Num--;
+            Texto.text = "" + Num;
+        }
+    }
+}
